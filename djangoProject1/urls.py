4c4b31d3c487316ab1491e5f5e3021f4d32a589d
Vladimir <djobirov05@mail.ru>
2024-12-26 14:58:37 +0500

    Add urls.py and wsgi.py from djangoProject

---
+++ djangoProject1/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path, include
+from main import views  # Импортируем views, если хотим использовать отдельное представление
+
+urlpatterns = [
+    path('admin/', admin.site.urls),  # Админка
+    path('index/', include('main.urls')),  # Путь для приложения main
+    path('', views.index, name='home'),  # Главная страница
+]
